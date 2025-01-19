a706441a7d4547109af9d71291028568d7845da6
Your Name <you@example.com>
2025-01-19 21:09:45 +0600

    feature/<branch>

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+
+import Tour from "@/components/admin/tours/Tour";
+
+
+
+export default function Home() {
+    return <div > <Tour/> </div>;
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/admin/tours/Tour.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Tours from '@/components/admin/tours/Tour'
+
+const Tour = () => {
+  return (
+    <><div  className=''></div> </>
+  );
+};
+
+export default Tour;
